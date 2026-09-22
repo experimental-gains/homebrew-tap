71cede3a6b8919a41a5655887cc47adb3660dd8d
self <self@users.noreply.github.com>
2026-09-22 06:30:48 +0000

    modslop 0.2.3

--- Formula/modslop.rb
+++ Formula/modslop.rb
@@ -1,8 +1,8 @@
 class Modslop < Formula
   desc "Catch slopsquatted and hallucinated Go module names in go.mod"
   homepage "https://github.com/experimental-gains/modslop"
-  url "https://github.com/experimental-gains/modslop/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "264f2be2cc798a07bec0400d37224f6a56b6f0c10aa9d27703dcecbbe01b5ff5"
+  url "https://github.com/experimental-gains/modslop/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "98829709f4ece1637674fed1bf4ad0e78241fc43e670777b52fe64f177597886"
   license "MIT"
   head "https://github.com/experimental-gains/modslop.git", branch: "main"
 
